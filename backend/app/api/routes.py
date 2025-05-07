fefc65136bf933656c134f9c0b0bda846af3e384
Sami Taider <taidersami1@gmail.com>
2025-05-07 17:05:10 +0200

    feat - landing page (#3)
    Co-authored-by: Djalil Chikhi <djalilchiki@gmail.com>

---
+++ backend/app/api/routes.py
@@ -0,0 +1,15 @@
+from fastapi import APIRouter
+
+router = APIRouter()
+
+@router.get("/main_window/local-models")
+async def get_local_models():
+    return {"message": "This is the Local Models endpoint."}
+
+@router.get("/main_window/available-models")
+async def get_available_models():
+    return {"message": "This is the Available Models endpoint."}
+
+@router.get("/main_window/train-new-model")
+async def train_new_model():
+    return {"message": "This is the Train New Model endpoint."}
