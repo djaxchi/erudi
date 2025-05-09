a61f0723905883208aa8092eebc6e931640da47d
Djalil Chikhi <djalilchiki@gmail.com>
2025-05-09 15:51:53 +0200

    fix - training page front feat - folder input

--- frontend/src/components/InfoRow.jsx
+++ frontend/src/components/InfoRow.jsx
@@ -4,7 +4,7 @@ export default function InfoRow({ label, children }) {
     return (
       <div className="flex justify-between items-center py-1">
         <span className="text-gray-200 font-medium w-1/2">{label}</span>
-        <div className="w-1/2 flex justify-end text-white">{children}</div>
+        <div className="w-1/2 flex justify-end text-right text-white">{children}</div>
       </div>
     );
   }
