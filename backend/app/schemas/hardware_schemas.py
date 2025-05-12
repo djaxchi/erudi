fad23b857c2020360be30e9575b18a84fb697093
Youssef <youschaouki@gmail.com>
2025-05-12 14:55:10 +0200

    fix - getting and displaying the hardware's components

--- backend/app/schemas/hardware_schemas.py
+++ backend/app/schemas/hardware_schemas.py
@@ -1,12 +1,24 @@
 from pydantic import BaseModel
+<<<<<<< HEAD
 from typing import Optional
+=======
+from typing import Optional  # Ajout de Optional
+>>>>>>> 6851920 (fix - getting and displaying the hardware's components)
 
 class HardwareInfo(BaseModel):
     total_ram_gb: float
     available_ram_gb: float
     cpu_model: str
+<<<<<<< HEAD
     gpu_model: Optional[str]
     disk_total_gb: float
     disk_available_gb: float
     cuda_installed: bool
     cuda_path: Optional[str]
+=======
+    gpu_model: Optional[str]  
+    disk_total_gb: float
+    disk_available_gb: float
+    cuda_installed: bool
+    cuda_path: Optional[str]  
+>>>>>>> 6851920 (fix - getting and displaying the hardware's components)
