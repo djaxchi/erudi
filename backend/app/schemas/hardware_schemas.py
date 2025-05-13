db48c50bbc2889ed0893bb4f25f326a00a0a7550
Youssef Chaouki <ychaouki@insa-lyon.fr>
2025-05-13 10:43:23 +0200

    rebase with main

--- backend/app/schemas/hardware_schemas.py
+++ backend/app/schemas/hardware_schemas.py
@@ -1,36 +1,13 @@
 from pydantic import BaseModel
-<<<<<<< HEAD
-<<<<<<< HEAD
 from typing import Optional
-=======
-from typing import Optional  # Ajout de Optional
->>>>>>> 6851920 (fix - getting and displaying the hardware's components)
-=======
-from typing import Optional  # Ajout de Optional
->>>>>>> 6851920b75b9119a6db094e330d68e3ceae74e63
+
 
 class HardwareInfo(BaseModel):
     total_ram_gb: float
     available_ram_gb: float
     cpu_model: str
-<<<<<<< HEAD
-<<<<<<< HEAD
-    gpu_model: Optional[str]
-    disk_total_gb: float
-    disk_available_gb: float
-    cuda_installed: bool
-    cuda_path: Optional[str]
-=======
     gpu_model: Optional[str]  
     disk_total_gb: float
     disk_available_gb: float
     cuda_installed: bool
     cuda_path: Optional[str]  
->>>>>>> 6851920 (fix - getting and displaying the hardware's components)
-=======
-    gpu_model: Optional[str]  
-    disk_total_gb: float
-    disk_available_gb: float
-    cuda_installed: bool
-    cuda_path: Optional[str]  
->>>>>>> 6851920b75b9119a6db094e330d68e3ceae74e63
